1fe1ff29eb8515a247e3887231e8dc4ea84d919a
SRocken <65569622+SRocken@users.noreply.github.com>
2020-05-29 15:02:35 -0400

    Added While Loop

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -28,19 +28,15 @@ products = [
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
    
+while True:
+    UPC = input("Please input the product number: ")
+    if UPC == "Done":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(UPC)]
+        matching_product = matching_products[0]
+    
 
-UPC = input("Please input the product number: ")
-matching_products = [p for p in products if str(p["id"]) == str(UPC)]
-matching_product = matching_products[0]
-
-#UPC = 1
-#while UPC == 1:
-#    UPC = input("Please input the product number: ")
-#    UPC = float(UPC)
-#    if UPC = num:
-#        pass
-#    else:
-#        break
 
 print("--------------------------------")
 print("        Mr Mango Grocery        ")
