2a348f3e663032eb7f8e906522b4d021f2c74e83
SRocken <65569622+SRocken@users.noreply.github.com>
2020-06-01 17:10:08 -0400

    Integrated Google Sheet

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -1,29 +1,31 @@
 # shopping_cart.py
 
+
 import datetime
 
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+# Google Sheets Integration (lines 7-26)
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# use creds to create a client to interact with the Google Drive API
+AUTH_SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
+    "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name('google_api_credentials.json', AUTH_SCOPE)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open a sheet
+os.environ["GOOGLE_SHEET_ID"] = "1ItN7Cc2Yn4K90cMIsxi2P045Gzw0y2JHB_EkV4mXXpI"
+DOCUMENT_ID = os.environ.get("GOOGLE_SHEET_ID", "OOPS")
+SHEET_NAME = os.environ.get("Shopping Cart Project - Datastore", "products")
+doc = client.open_by_key(DOCUMENT_ID)
+sheet = doc.worksheet(SHEET_NAME)
+
+# Pull Google Sheet values into the application
+products = sheet.get_all_records()
 
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
@@ -60,7 +62,8 @@ for UPC in UPCs:
     matching_product = matching_products[0]
     subtotal_price = subtotal_price + matching_product["price"]
     matching_product_price = to_usd(matching_product["price"])
-    print("  " + matching_product["name"] + " " + str(matching_product_price))
+    matching_prouduct_count = len(matching_product)
+    print("  ..." + matching_product["name"] + " " + str(matching_product_price))
 
 tax = subtotal_price * .0875
 total_price = subtotal_price + tax
@@ -80,3 +83,7 @@ print(" ")
 print("------------------------------------------")
 print("          THANKS, SEE YOU AGAIN!          ")
 print("------------------------------------------")
+
+# TODO: add product count and have multiple products in same line with price reflecting that
+# TODO: connect to google sheets
+# TODO: connect to email
